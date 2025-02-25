5bb5c5311a023d985df1ee493254adbc408c84d3
del-mperalta <110034@global.ul.com>
2025-02-25 08:14:54 -0500

    sdfa

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -5,7 +5,41 @@ import {
 import Experience from '@ball/app/_components/Experience/Experience';
 import { getClient } from '@ball/server/contentfulClient';
 import { experienceTypeId } from '@ball/env';
-import '@ball/registeredComponents';
+
+import {
+  CONTENTFUL_COMPONENTS,
+  defineComponents,
+} from "@contentful/experiences-sdk-react";
+import { Button } from "@ball/app/_components/Button/button";
+
+defineComponents(
+  [
+    {
+      component: Button,
+      definition: {
+        id: "button",
+        name: "Button",
+        category: "Custom Components",
+        variables: {
+          text: {
+            displayName: "Text",
+            type: "Text",
+            defaultValue: "Click me!",
+          },
+        },
+      },
+    },
+  ],
+  {
+    enabledBuiltinComponents: [
+      CONTENTFUL_COMPONENTS.button.id,
+      CONTENTFUL_COMPONENTS.divider.id,
+      CONTENTFUL_COMPONENTS.heading.id,
+      CONTENTFUL_COMPONENTS.richText.id,
+      CONTENTFUL_COMPONENTS.text.id,
+    ],
+  },
+);
 
 const client = getClient();
 
